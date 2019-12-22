dd6d0166b11135a7ddb6708cf179e1d28523e1ad
mrrobotbrz <caio.ferraz@souunit.com.br>
2019-12-22 16:27:34 -0300

    Criando models  e instalando entity.

---
+++ ApiCursos/Models/Curso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCursos.Models
+{
+    public class Curso
+    { 
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O titulo precisa ser preenchido.")]
+        [MaxLength(50)]
+        public String Titulo { get; set; }
+
+        public String Url { get; set; }
+        [Required(ErrorMessage = "a data de publicação deve ser preenchida")]
+        public DateTime datapublicacao { get; set; }
+        [Required(ErrorMessage = "campo requirido")]
+        [Range(1,1000)]
+        public int CargaHoraria { get; set; }
+    }
+}
