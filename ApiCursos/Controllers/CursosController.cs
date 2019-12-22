8090d7ad352b1129d79cacb286af0cb76291510a
mrrobotbrz <caio.ferraz@souunit.com.br>
2019-12-22 17:02:03 -0300

    configurando web config e criando controller

---
+++ ApiCursos/Controllers/CursosController.cs
@@ -0,0 +1,19 @@
+﻿using ApiCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiCursos.Controllers
+{
+    public class CursosController : Controller
+    {
+        private ProjetoContext db = new ProjetoContext();
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApiCursos/Models/ProjetoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiCursos.Models
+{
+    public class ProjetoContext : DbContext
+    {
+        public ProjetoContext() : base("projetolocal")
+        {
+                
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+
+    }
+}
