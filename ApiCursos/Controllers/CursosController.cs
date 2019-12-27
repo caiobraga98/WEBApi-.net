dcb1fbbd1554daab137260e1c10c84d08669a72b
mrrobotbrz <caio.ferraz@souunit.com.br>
2019-12-27 05:45:05 -0300

    adcionando elementos a controller

--- ApiCursos/Controllers/CursosController.cs
+++ ApiCursos/Controllers/CursosController.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace ApiCursos.Controllers
@@ -11,9 +13,28 @@ namespace ApiCursos.Controllers
     {
         private ProjetoContext db = new ProjetoContext();
         // GET: Cursos
-        public ActionResult Index()
+         public IHttpActionResult PostCurso(Curso curso)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+ 
+            db.Cursos.Add(curso);
+            db.SaveChanges();
+ 
+            return CreatedAtRoute("DefaultApi", new { id = curso.Id }, curso);
+        }
+ 
+        public IHttpActionResult GetCurso(int id)
+        {
+            if (id <= 0)
+                return BadRequest("O id deve ser um numero maior que zero.");
+ 
+            var curso = db.Cursos.Find(id);
+ 
+            if (curso == null)
+                return NotFound();
+ 
+            return Ok(curso);
         }
     }
 }
